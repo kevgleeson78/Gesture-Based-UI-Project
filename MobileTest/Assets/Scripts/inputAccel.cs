ecff5ad105d26a99659c9aa468a443f2126fe0c0
Kevin <Kevin@DESKTOP-12A507O>
2019-03-05 17:50:37 +0000

    sphere added for testing

---
+++ MobileTest/Assets/Scripts/inputAccel.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class inputAccel : MonoBehaviour {
+    private Rigidbody rigid;
+	// Use this for initialization
+	void Start () {
+        rigid = GetComponent<Rigidbody>();
+	}
+   
+    // Update is called once per frame
+    void Update()
+    {
+      
+        rigid.AddForce(Input.acceleration);
+    }
+}
