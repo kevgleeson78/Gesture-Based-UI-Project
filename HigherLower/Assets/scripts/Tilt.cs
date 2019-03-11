7b0b9c21984f17be2bd0725ae7d3e9510b45d294
Kevin <Kevin@DESKTOP-12A507O>
2019-03-11 17:08:49 +0000

    getting angle of gyroscope test

---
+++ HigherLower/Assets/scripts/Tilt.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tilt : MonoBehaviour {
+    private void Update()
+    {
+        GetZRotation();
+    }
+
+    void GetZRotation()
+    {
+        Quaternion referenceRotation = Quaternion.identity;
+        Quaternion deviceRotation = new Quaternion(0.5f, 0.5f, -0.5f, 0.5f) * Input.gyro.attitude * new Quaternion(0, 0, 1, 0);
+        Quaternion eliminationOfXY = Quaternion.Inverse(
+            Quaternion.FromToRotation(referenceRotation * Vector3.forward,
+                                  deviceRotation * Vector3.forward)
+            );
+        Quaternion rotationZ = eliminationOfXY * deviceRotation;
+        float roll = rotationZ.eulerAngles.z;
+
+        print(roll);
+    }
+}
+    
