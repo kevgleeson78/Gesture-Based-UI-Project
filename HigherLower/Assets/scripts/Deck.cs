a7c993a1578743e8d42885be5017f79c04b9b643
Kevin <Kevin@DESKTOP-12A507O>
2019-03-12 17:26:36 +0000

    random shuffle added

---
+++ HigherLower/Assets/scripts/Deck.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    List<int> deck;
+    public void Shuffle()
+    {
+        if(deck == null)
+        {
+            deck = new List<int>();
+        }
+        else
+        {
+            deck.Clear();
+        }
+
+        for (int i = 0; i < 52; i++)
+        {
+            deck.Add(i);
+        }
+
+        int n = deck.Count;
+        while (n > 1)
+        {
+
+            n--;
+            int k = random.Next(n);
+            n--;
+            T temp = list[k];
+            list[k] = list[n];
+            list[n] = temp;
+        }
+    }
+
+    void Start()
+    {
+        Shuffle();
+    }
+
+}
