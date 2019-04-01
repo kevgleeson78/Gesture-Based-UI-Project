c6be8773489818e92f2b09a6919cc0318077c681
Kevin <Kevin@DESKTOP-12A507O>
2019-04-01 15:41:01 +0100

    more comments added to CardStack.cs

--- HigherLower/Assets/scripts/CardEventDelegate.cs
+++ HigherLower/Assets/scripts/CardEventDelegate.cs
@@ -1,3 +1,3 @@
 ﻿using System;
-
+//Event handler for the card stack
 public delegate void CardEventHandler(Object sender, CardEventArgs e);
--- HigherLower/Assets/scripts/CardStack.cs
+++ HigherLower/Assets/scripts/CardStack.cs
@@ -4,16 +4,20 @@ using UnityEngine;
 
 public class CardStack : MonoBehaviour
 {
+    // List to hold crad index
     List<int> cards;
+
     public bool isGameDeck;
     public bool HasCards
     {
         get { return cards != null && cards.Count > 0; }
     }
 
-    //Public event
+    //Public event card removed from main deck
     public event CardEventHandler CardRemoved;
+    // Card added to new deck, player aor dealer hand
     public event CardEventHandler cardAdded;
+    // GEt the amount of cards in the list
     public int CardCount
     {
         get
@@ -28,6 +32,7 @@ public class CardStack : MonoBehaviour
             }
         }
     }
+    // Get adn return the cards in the cards list
     public IEnumerable<int> GetCards()
     {
         foreach (int i in cards)
@@ -35,9 +40,11 @@ public class CardStack : MonoBehaviour
             yield return i;
         }
     }
+    // To remove a card from teh main deck
     public int Pop()
     {
         int temp = cards[0];
+        // Remove first card from the stack
         cards.RemoveAt(0);
 
         if (CardRemoved != null)
@@ -46,7 +53,7 @@ public class CardStack : MonoBehaviour
         }
         return temp;
     }
-
+    // to put a new card to the player or dealer hand
     public void  Push(int card)
     {
         cards.Add(card);
@@ -55,76 +62,102 @@ public class CardStack : MonoBehaviour
             cardAdded(this, new CardEventArgs(card));
         }
     }
+    // Set the value of the cards
     public int HandValue()
     {
+        // Keep track of the cards
         int total = 0;
         int aces = 0;
+        //loop through the card array.
         foreach(int card in GetCards())
         {
+            // All card are stored from ace to king in each suit
+            // % 13 will split them up into indvidual values
             int cardRank = card % 13;
+            // 0 = ace 
+            // Get values from 2 - 9 excluding ace, king, queen  and jack
             if(cardRank <= 9 && cardRank > 0)
             {
                 cardRank += 1;
+                // total the value
+                // This is use to calculate the player and dealer score
                 total = total + cardRank;
             }
+            // set the values for king, queen , jack (all = 10).
             else if(cardRank >9 && cardRank <= 12)
             {
                 cardRank = 10;
+                // Add to the total score
                 total = total + cardRank;
             }
             else
             {
+                // Count the aces
                 aces++;
             }
 
             
         }
+        // for the different possible values the ace can have either a one or eleven.
         for (int i = 0; i < aces; i++)
         {
+            // check if the the current score is less than or equal to 21
             if (total+11 <= 21)
             {
+                // Ace == 11 if above condition is true
                 total = total + 11;
             }
             else
             {
+                // Ace is worth one if false.
                 total = total + 1;
             }
         }
 
-
+        // The final score
         return total;
     }
+    // Creating the shuffled deck of cards
     public void CreateDeck()
     {
-        
+        // Clear the cards array
         cards.Clear();
      
-
+        //Add the 1 - 52 to the cards array
         for (int i = 0; i < 52; i++)
         {
             cards.Add(i);
         }
 
         int n = cards.Count;
+        // Fisher yates shuffle
         while (n > 1)
         {
+            //decrement the counter by one
             n--;
+            // Pick a ranndom number between the current size of the array.
             int k = Random.Range(0, n+1);
-           
+            //store random value 
             int temp = cards[k];
+            // assign to 52nd slot on first pass the random value to n. 
+            //(n will decrease by one after ech recursive call)  
             cards[k] = cards[n];
+            // Finally add the random index to the cards array
             cards[n] = temp;
         }
     }
+    // Reset for new gamme
     public void Reset()
     {
         cards.Clear();
     }
     void Awake()
     {
+        // Create new list on new game
         cards = new List<int>();
         if (isGameDeck)
         {
+            // Create a new deck.
             CreateDeck();
         }
        
